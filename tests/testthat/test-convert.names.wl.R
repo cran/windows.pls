e14a0229c77af9e37ada36bf364d73b2cf7fc7bc
Elia Gonzato <elia.gonzato@outlook.it>
2023-08-09 17:30:02 +0000

    version 0.1.0

---
+++ tests/testthat/test-convert.names.wl.R
@@ -0,0 +1,12 @@
+data("beer")
+conc=unlist(beer[,1])
+spectra=beer[,2:ncol(beer)]
+names(spectra)=convert.names.wl(1100,2250,2)
+
+#
+test_that(desc="The best window is Window1",code ={
+  name4=names(spectra)[4];
+  expect_equal(object=name4,expected='X1106');
+  expect_equal(object = is.character(name4),expected = TRUE)
+
+} )
